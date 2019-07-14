ba5e8eca7a536838657e63447b6841aa1b142347
刘佳伊 <simplelive2013@126.com>
2019-07-15 07:59:41 +0800

    Create start.py

---
+++ start.py
@@ -0,0 +1,6 @@
+import sys,os
+from ..core import src
+BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.appMAX_VOCAB_SIZEend(BASE_DIR)
+if __name__=='__main__':
+    src.run()
